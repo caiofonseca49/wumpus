9d47791ffce9027704ac8cc0040d428ee61e53ff
caiofonseca49 <66683891+caiofonseca49@users.noreply.github.com>
2022-11-28 08:38:46 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = uuid4().hex
 
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "null"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 socketio = SocketIO(app)
